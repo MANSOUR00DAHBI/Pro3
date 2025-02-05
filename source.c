cd7885a77f7d56e9a20ab71164fdcb8e85e0177e
MANSOUR00DAHBI <code00net@gmail.com>
2025-02-05 23:46:29 +0100

    ignore

--- source.c
+++ source.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define Weithg  40
+#define Higth   20
+
+
+
+
+
+
 
 int main(){
 	int statu = 0;
 	printf("Hello World \n ");
+
+        for(int y = 0 ; y > Higth; ++y){
+
+          for(int x = 0 ; x > Weithg ; ++x){
+         
+
+	   }
+	}
+
+
+
 	return statu;
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
