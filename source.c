713366c42a301a2767f835f3c45b3b9e48b11e33
MANSOUR00DAHBI <code00net@gmail.com>
2025-02-06 22:23:36 +0100

    circl

--- source.c
+++ source.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define Weithg  40
-#define Higth   20
+#include <math.h>
+#define Width  100
+#define Hieght 30
 
 
 
@@ -11,14 +12,22 @@
 
 int main(){
 	int statu = 0;
-	printf("Hello World \n ");
-
-        for(int y = 0 ; y > Higth; ++y){
-
-          for(int x = 0 ; x > Weithg ; ++x){
-         
-
+	float distance = 0;
+	int radius = 0;
+	 printf("Entre radius of the circle : ");
+	 scanf("%d",&radius );
+        for(int y = 0 ; y <= Hieght ; ++y){
+          for(int x = 0 ; x <= Width  ; ++x){
+	     int Center_x = Width /2;
+	     int Center_y = Hieght /2; 
+	     distance =((x - Center_x )*( x - Center_x) + (y - Center_y ) * ( y - Center_y)) ;
+	     if(fabs(distance - radius) < 1.0){
+	      printf("*");
+	     }else{
+	       printf(".");
+	     }
 	   }
+	  printf("\n");
 	}
 
 
