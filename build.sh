bbf821531717ad9fc5ea4c8c66d518c30bddc316
MANSOUR00DAHBI <code00net@gmail.com>
2025-02-07 03:24:08 +0100

    Circl 3

--- build.sh
+++ build.sh
@@ -3,7 +3,18 @@ echo "Started Compler "
 set -xe 
 
 gcc -o build/Setup source.c -Wall -Wextra -lm 
-gcc -o build/detab detab.c  -Wall -Wextra -lm 
-gcc -o build/tab tab2.c  -Wall -Wextra -lm 
-gcc -o build/tab3 det3.c  -Wall -Wextra -lm 
-echo "End Compiler"
+#gcc -o build/detab detab.c  -Wall -Wextra -lm 
+#gcc -o build/tab3 det3.c  -Wall -Wextra -lm
+#gcc -o build/CM Circl_Key_move.c  -Wall -Wextra -lm
+#gcc -o build/C_W Circl_win.c  -Wall -Wextra -lm
+#gcc -o build/C_M1 Circl1_move.c  -Wall -Wextra -lm
+#gcc -o build/C_W1 Circl1_win.c  -Wall -Wextra -lm
+#gcc -o build/Wind win.c  -Wall -Wextra -lm
+#gcc -o build/W2 win2.c  -Wall -Wextra -lm
+
+
+
+
+
+
+ echo "End Compiler"
--- source.c
+++ source.c
@@ -2,40 +2,67 @@
 #include <stdlib.h>
 #include <math.h>
 #include <windows.h>
+#include <unistd.h>
 #define Width  100
 #define Hieght 30
 
+struct Circle {
+	int x, y, r, xx, yy;
+};
+void clear_screen() {
+	//printf("\e[2J");
+	//printf("\e[H");
+	system("clear");
+}
 
+void Draw_Circle(struct Circle circle) {
+	for (int y = 0; y <= Hieght; ++y) {
+		for (int x = 0; x <= Width; ++x) {
 
+			float distance = sqrt(pow(x - circle.x, 2) + pow(y - circle.y, 2));
+			if(distance  < circle.r){
+				 printf("*");
+			}
+			else {
+				printf(".");
+			}
 
+		}
+		printf("\n");
+	}
+}
+void setup(struct Circle* circle) {
+	circle->x += circle->xx;
+	circle->y += circle->yy;
+	if (circle->x - circle->r < 0) {
+		circle->xx = -circle->xx;
+	}
+	if (circle->x + circle->r > Width) {
+		circle->xx = -circle->xx;
+	}
+	if (circle->y - circle->r < 0) {
+		circle->yy = -circle->yy;
+	}
+	if (circle->y + circle->r > Hieght) {
+		circle->yy = -circle->yy;
+	}
 
-
+}
 
 int main(){
 	int statu = 0;
-	float distance = 0;
-	int radius = 0;
-	 printf("Entre radius of the circle : ");
-	 scanf("%d",&radius );
-        for(int y = 0 ; y <= Hieght ; ++y){
-          for(int x = 0 ; x <= Width  ; ++x){
-	     int Center_x = Width /2;
-	     int Center_y = Hieght /2; 
-	     distance =((x - Center_x )*( x - Center_x) + (y - Center_y ) * ( y - Center_y)) ;
-	     if(fabs(distance - radius) < 1.0){
-	      printf("*");
-	     }else{
-	       printf(".");
-	     }
-	   }
-	  printf("\n");
+	struct Circle circle = { 10,10,5,1,1 };
+	while (1) {
+		 clear_screen();
+	      Draw_Circle(circle);
+		 setup(&circle);
+
+		  usleep(500000);
 	}
-
-
-
+	printf("\n");
+	
+	
 	return statu;
-
-
 }
 
 
